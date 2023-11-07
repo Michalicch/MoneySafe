19e13f8caa5abac301765eea0c3070eed520a091
Michalicch <lorka.diskaveri@gmail.com>
2023-11-07 22:14:40 +0300

    1 получение данных из инпутов, наложение функционала на кнопки

---
+++ js/script.js
@@ -0,0 +1,39 @@
+'use strict'
+
+import { convertStringNumber } from "./convertStringNumber.js";
+
+const financeForm = document.querySelector(".finance__form");
+const financeAmount = document.querySelector(".finance__amount");
+const financeReport = document.querySelector(".finance__report");
+const report = document.querySelector(".report"); 
+const reportClose = document.querySelector(".report__close");
+
+let amount = 0;
+financeAmount.textContent = amount;
+
+financeForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const typeOperation = e.submitter.dataset.typeOperation;
+	
+	const changeAmount = Math.abs(convertStringNumber(financeForm.amount.value)); 
+		
+	if(typeOperation === 'income') {
+		amount += changeAmount;
+	}
+
+	if(typeOperation === 'expenses') {
+		amount -= changeAmount;
+	}
+	
+	financeAmount.textContent = `${amount.toLocaleString()} ₽`;
+
+});
+
+financeReport.addEventListener('click', () => {
+	report.classList.add('report__open')
+})
+
+reportClose.addEventListener('click', () => {
+	report.classList.remove('report__open')
+})
+ 
